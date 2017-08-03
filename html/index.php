16313c2f9afc0eda155d2ad8f7628b3b96c1435d
qkrwls5998 <qkrwls5998@gmail.com>
2017-08-03 21:09:31 +1200

    index.php
    homepage

---
+++ html/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <div id="mySidenav" class="sidenav">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+    </div>
+
+    <header>
+        <h1><a href="http://localhost/my_project/my_project/html/index.php">Jinyoung Park</a></h1>
+        <h2>Software Developer</h2>
+    </header>
+
+
+</body>
+</html>
